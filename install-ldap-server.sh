d13a9c79017e66713061bad1ec31cb155e0b5fb4
Thomas <thomasooo54@gmail.com>
2020-04-05 14:27:07 -0400

    Add files via upload
    changed IP address in line 39 to the IP of the group server

--- install-ldap-server.sh
+++ install-ldap-server.sh
@@ -14,7 +14,7 @@ wget -O /etc/openldap/base.ldif https://raw.githubusercontent.com/Thomaso54/Grou
 hash=$(slappasswd -s RootGroup3 -n) >> ldap-server.log
 sed -i "s/olcRootPW:/olcRootPW: $hash/g" /etc/openldap/db.ldif >> ldap-server.log
 echo "olcAccess: {0}to attrs=userPassword, by self write by anonymous auth by * none" >> /etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif
-    echo "olcAccess: {1} to * by self write by * read" >> /etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif
+echo "olcAccess: {1} to * by self write by * read" >> /etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif
 #Enable slapd and start it
 systemctl enable slapd.service && systemctl start slapd >> ldap-server.log
 #Set firewall rules
@@ -36,7 +36,7 @@ wget --directory-prefix=/usr/local/ http://www.hits.at/diradm/diradm-1.3.tar.gz 
 #Unzip diradm directory
 tar zxvf /usr/local/diradm-1.3.tar.gz -C /usr/local/ >> ldap-server.log
 #Create the diradm.conf file using printf
-printf "# Begin /etc/diradm.conf\n# LDAP specific options\n# ---------------------\nLDAPURI="ldap://10.2.7.72:389/"\nBINDDN="cn=Manager,dc=cit470,dc=nku,dc=edu"\n# Be extremely careful with read rights\n# of this file if you set this value!!!\n# BINDPASS="secret"\nUSERBASE="ou=People,dc=cit470,dc=nku,dc=edu"\nGROUPBASE="ou=Group,dc=cit470,dc=nku,dc=edu"\n# Options for user accounts\n# ---------------------------------\nUIDNUMBERMIN="1000"\nUIDNUMBERMAX="60000"\nUSERGROUPS="yes"\nHOMEBASE="/home"\nHOMEPERM="0750"/nSKEL="/etc/skel"\nDEFAULT_GIDNUMBER="100"\nDEFAULT_LOGINSHELL="/bin/bash"\nDEFAULT_SHADOWINACTIVE="7"\nDEFAULT_SHADOWEXPIRE="-1"\nSHADOWMIN="0"\nSHADOWMAX="99999"\nSHADOWWARNING="7"\nSHADOWFLAG="0"\n# Options for group accounts\n# ----------------------------------\nGIDNUMBERMIN="1000"\nGIDNUMBERMAX="60000"\n# End /etc/diradm.conf" >> diradm.conf
+printf "# Begin /etc/diradm.conf\n# LDAP specific options\n# ---------------------\nLDAPURI="ldap://10.2.7.15:389/"\nBINDDN="cn=Manager,dc=cit470,dc=nku,dc=edu"\n# Be extremely careful with read rights\n# of this file if you set this value!!!\n# BINDPASS="secret"\nUSERBASE="ou=People,dc=cit470,dc=nku,dc=edu"\nGROUPBASE="ou=Group,dc=cit470,dc=nku,dc=edu"\n# Options for user accounts\n# ---------------------------------\nUIDNUMBERMIN="1000"\nUIDNUMBERMAX="60000"\nUSERGROUPS="yes"\nHOMEBASE="/home"\nHOMEPERM="0750"/nSKEL="/etc/skel"\nDEFAULT_GIDNUMBER="100"\nDEFAULT_LOGINSHELL="/bin/bash"\nDEFAULT_SHADOWINACTIVE="7"\nDEFAULT_SHADOWEXPIRE="-1"\nSHADOWMIN="0"\nSHADOWMAX="99999"\nSHADOWWARNING="7"\nSHADOWFLAG="0"\n# Options for group accounts\n# ----------------------------------\nGIDNUMBERMIN="1000"\nGIDNUMBERMAX="60000"\n# End /etc/diradm.conf" >> diradm.conf
 #Move and overwrite  diradm.conf with the one created
 mv -f diradm.conf /usr/local/diradm-1.3/ >> ldap-server.log
 cd /usr/local/diradm-1.3
